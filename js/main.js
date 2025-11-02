0bd936e63453220b1b45e49ab07488ac658cc61c
Tincho <martin.negri@outlook.es>
2025-11-02 03:17:27 -0300

    Carrusel y footer

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const carousel = document.querySelector('.carousel');
+    const next = document.querySelector('.carousel-btn.next');
+    const prev = document.querySelector('.carousel-btn.prev');
+
+    if (carousel && next && prev) {
+        const scrollAmount = carousel.offsetWidth;
+
+        next.addEventListener('click', () => {
+        carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+        });
+
+        prev.addEventListener('click', () => {
+        carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+        });
+    }
+    });
